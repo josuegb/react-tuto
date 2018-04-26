60af98ae68d070d6e73d62ced251bea7146aeb6e
josuegb15 <josueg@thehangar.cr>
2018-04-26 14:17:15 -0600

    Validating Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass'
@@ -34,6 +36,13 @@ class Person extends Component {
     //   <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
     // ];
   }
-};
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+}
 
 export default withClass(Person, classes.Person);
